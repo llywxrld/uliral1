d881962e3744fd598f5e62317677409efea1f47c
username <email>
2024-12-13 11:45:25 +0800

    okaay

--- pages/lily4.js
+++ pages/lily4.js
@@ -1,24 +1,57 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
+export default function Lily4() {
+    const [grid, setGrid] = useState(false);
 const data = [
     {
         id: 1,
-        title: "test 1"
-    }
-]
+        title: "Test 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis nulla eu eros vestibulum tristique. Duis et quam et dolor condimentum."
+    },
+    {
+        id: 2,
+        title: "Test 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis nulla eu eros vestibulum tristique. Duis et quam et dolor condimentum."
+    },
+    {
+        id: 3,
+        title: "Test 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis nulla eu eros vestibulum tristique. Duis et quam et dolor condimentum."
+    },
+    {
+        id: 4,
+        title: "Test 4",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis nulla eu eros vestibulum tristique. Duis et quam et dolor condimentum."
+    },
+    {
+        id: 5,
+        title: "Test 5",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis nulla eu eros vestibulum tristique. Duis et quam et dolor condimentum."
+    },
+    {
+        id: 6,
+        title: "Test 6",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis nulla eu eros vestibulum tristique. Duis et quam et dolor condimentum."
+    },
+];
 
-export default function Lily4(){
-    const [grid, setGrid]  = useState(false);
     return(
-        <div className="font-bold text-xl">
-            <div className="flex justify-between mx-80">
-                <p>Нийтлэлүүд</p>
-                <button onClick={() => setGrid(!grid)} 
-                className=" h-12 w-48  bg-blue-500 rounded-md text-sm font-normal ">{grid == true ? "Жагсаалт харах руу шилжэх" : "Grid-руу шилжэх"}</button>
+        <div className="w-full h-screen bg-white px-80 ">
+            <div className="flex justify-between mt-4 mx-8">
+                <p className="text-2xl font-bold">Нийтлэлүүд</p>
+                <button className="border-2 border-red-300 rounded-lg p-4 mb-4 text-2xl text-blue-500 font-bold" onClick={() => setGrid(!grid)}>{grid == false ? "Grid-руу шилжих" : "Жагсаалтруу шилжих"}</button>
             </div>
-            <div>
-
+            
+            <div className={(grid ==false ? "w-full text-black mt-4" : "w-full text-black grid grid-cols-2 gap-x-8 mt-4" )}>
+                {data?.map((data, index) => (
+                    <div key={index}>
+                        <div className="border-2 border-black rounded-lg p-4 mb-4">
+                            <p className="text-sm font-bold">{data.title}</p>
+                            <p className="mt-4">{data.description}</p>
+                        </div>
+                    </div>
+                ) )}
             </div>
         </div>
-    )    
-}                                 
+    )
+} 
---
+++ pages/lily5.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export default function task4() {
+  const [search, setSearch] = useState("");
+  console.log("search utga -->", search);
+  return (
+    <div className="bg-white">
+      <div
+        className="flex items-center justify-center 
+              h-screen flex-col        "
+      >
+        <div className=" border border-black w-[400px] h-[500px]">
+          <div className="flex justify-center items-center mt-2 flex-col ">
+            <img className="w-24 h-24" src="https://static.vecteezy.com/system/resources/previews/018/930/468/original/pinterest-logo-pinterest-transparent-free-png.png"/>
+            <div>
+              <p className="text-3xl mb-20">Log in to see more</p>
+            </div>
+            <div className="flex ">
+              <input
+                type="search"
+                onChange={(test) => setSearch(test.value)}
+                placeholder="Email"
+                className="p-2 pr-24 bg-white border-2 border-black rounded-lg   focus:outlinenone focus:ring-2  focus:ring-blue-1   00"
+              ></input>
+            </div>
+            <div>
+              <input
+                type="search"
+                onChange={(test) => setSearch(test.value)}
+                placeholder="Password"
+                className=" mt-4 p-2 pr-24  bg-white border-2 border-gray-800 rounded-lg  focus:outlinenone focus:ring-2  focus:ring-blue-1   00"
+              ></input>
+            </div>
+            <div className="bg-blue-600 h-8 w-[310px] rounded-xl mt-4  ">
+              <h1 className=" flex items-center justify-center mt-1 ">
+                Log in
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
